ba2cf255675395b0469d1aee4c93260c984debd6
onkar <onkarsawant111@gmail.com>
2024-05-03 23:31:36 +0530

    updated

---
+++ functions.py
@@ -0,0 +1,47 @@
+# def cal(n1,n2):
+#     return n1+n2
+# print(cal(1,2))
+
+# def avg(n1,n2,n3):
+#     return (n1+n2+n3)/3
+# print(avg(1,5,6))
+
+# print("onkar",end=" ")
+# print("sawant")
+
+# a = [1,2,3,4,5]
+# def length(list):
+#     l = length(list)
+#     return l 
+# print(len(a))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
