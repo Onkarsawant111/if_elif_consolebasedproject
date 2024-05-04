8427782e31f67117e7844bfb4b42ec9e68648774
onkar <onkarsawant111@gmail.com>
2024-05-04 15:26:02 +0530

    updated

--- functions.py
+++ functions.py
@@ -15,6 +15,64 @@
 #     return l 
 # print(len(a))
 
+# a = ["onkar","raj","max","tony"]
+
+# def getdata(list):
+#     i = 0
+#     while i < len(list):
+#         print(list[i],end=" ")
+#         i = i + 1
+# getdata(a)
+
+
+# def fact(n):
+#     i = 1
+#     f = 1
+#     while i <= n:
+#         f = f * i
+#         i = i + 1
+#     return(f)
+
+# print(fact(int(input("no: "))))
+
+# def convert(n):
+#     inr = 80
+#     usd = inr * n 
+#     return usd
+# print(convert(int(input("usd: "))))
+
+# def show(n):
+#     if n == 0:
+#         return
+#     print(n)
+#     show(n - 1)
+
+# show(5)
+
+# def fact(n):
+#     print(n)
+#     if n == 0:
+#         return 1
+#     return n * fact(n-1)
+
+# print(fact(5))
+
+# def sum(n):
+#     if n == 0:
+#         return 0
+#     return n + sum(n -1)
+# print(sum(5))
+
+# a = ["raj","max","onkar","tony"]
+# def show(li , i):
+#     if i == len(li):
+#         return 
+#     print(li[i])
+#     show(li, i + 1)
+
+# show(a,0)
+
+
 
 
 
