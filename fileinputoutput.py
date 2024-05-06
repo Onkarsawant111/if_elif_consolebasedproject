613936d7c733d2e898766d14436a4d5d12c0ffea
onkar <onkarsawant111@gmail.com>
2024-05-06 12:01:56 +0530

    updated

--- fileinputoutput.py
+++ fileinputoutput.py
@@ -1,3 +1,53 @@
-f = open("demo.txt","w")
-data = f.write()
-print(data)
+# f = open("demo.txt","r")
+# data = f.readline()
+# print(data)
+
+# f = open("demo.txt","w")
+# f.write("gm")
+# f.close()
+
+# f = open("demo.txt","a")
+# f.write("onkar sawant")
+# f.close()
+
+# f = open("demo.txt","r+")
+# f.write("hello")
+# f.close()
+
+# f = open("demo.txt","w+")
+# f.write("onkar sawant")
+# f.close()
+
+# import os
+# os.remove("demo.txt")
+
+# q.1)
+# f = open("practice.txt","w+")
+# f.write("hi everyone\nwe are learning file i/o\nusing java\ni like programming in java")
+
+# f = open("practice.txt","r")
+# data = f.read()
+# newdata= data.replace("java","python")
+# print(newdata)
+
+# f = open("practice.txt","w")
+# f.write(newdata)
+
+# f = open("practice.txt","r")
+# data = f.read()
+# search = data.find("learning")
+# print(search)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
