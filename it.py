71ba359c63f0f68b8b705de2b301372efcc4a805
onkar <onkarsawant111@gmail.com>
2024-05-10 02:02:38 +0530

    updated

--- it.py
+++ it.py
@@ -96,12 +96,132 @@
 #     i = i - 1
 # print(s)
 
+# a = "my name is khan"
+# data = "aeiou"
+# v = 0
+# sp = 0
+# tletters = 0
+# i = 0
+# while i < len(a):
+#     if a[i].lower() in data:
+#         v = v + 1
+#     elif a[i] != data and a[i] != " ":
+#         tletters = tletters + 1
+#     else:
+#         sp = sp + 1
+#     i = i + 1
+# print("total no of vowels =", v)
+# print("total no of spaces =", sp)
+# print("total no of letters =", tletters)
 
+# a = ["onkar","raj","max","tony"]
+# i = 0 
+# while i < len(a):
+#     print(a[i])
+#     i = i +1 
 
+# for i in range(len(a)):
+#     print(a[i])
 
+# a = [2,45,6,1,39,7,5,4,6,8,8]
+# i = 0
+# while i < len(a):
+#     if a[i]%2 == 0:
+#         print(a[i])
+#     i = i + 1
 
+# a = [2,45,6,1,39,7,5,4,6,8,8]
+# i = 0
+# while i < len(a):
+#     m = i + 1
+#     while m < len(a):
+#         if a[i] == a[m]:
+#             print(a[i],end=" ")  
+#         m = m + 1
+#     i = i + 1
 
+# find max and min no in list 
 
+# a = [89,22,3,4449,5]
+# max = a[0]
+# i = 0
+# while i < len(a):
+#     if a[i] > max:
+#         max = a[i]
+#     i = i + 1 
+# print(max)
 
+# a = [89,22,3,4449,5,0.5]
+# min = a[0]
+# i = 0
+# while i < len(a):
+#     if a[i] < min:
+#         min = a[i]
+#     i = i + 1 
+# print(min)
 
+# find second highest no 
 
+# a = [7,2,3,7,78]
+# i = 0 
+# max = a[i]
+# min = a[i]
+# while i < len(a):
+#     if a[i] > max:
+#         max = a[i]
+#     elif a[i] < min:
+#         min = a[i]
+#     i = i + 1
+
+# j = 0
+# n = a[j]
+# smax = 0
+# while j < len(a):
+#     if max > a[j] > min:
+#         smax = a[j]
+#     j = j + 1
+
+# print(max)
+# print(min)
+# print(smax)
+
+# print
+# * * * * * 
+#   * 
+#     *
+#       *
+#         *
+
+# r = 1
+# while r <= 5:
+#     c = 1
+#     while c <= 5:
+#         if r == 1:
+#             print("*", end=" ")
+#         elif (r == 2 and c == 2) or (r == 3 and c == 3) or (r == 4 and c == 4) or (r == 5 and c == 5):
+#             print("*", end=" ")
+#         else:
+#             print(" ", end=" ")
+#         c = c + 1
+#     print()
+#     r = r + 1
+
+# print
+#   b
+# a   a
+# a b a
+# a   a
+
+# r = 1 
+# while r <= 4:
+#     c = 1
+#     while c <=3:
+#         if (r == 1 and c == 2) or (r == 3 and c == 2):
+#             print("b", end=" ")
+#         elif ((r == 2 or r == 3 or r == 4) and c == 1) or ((r == 2 or r == 3 or r == 4) and c == 3):
+#             print("a", end=" ")
+#         else:
+#             print(" ", end=" ")
+#         c = c + 1
+#     print()
+#     r = r + 1
